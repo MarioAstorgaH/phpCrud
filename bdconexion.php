a6d85f77f101dbaf2d66467a895536d0b27c2b67
Mario <astorgamhdez@gmail.com>
2025-03-05 13:21:02 -0700

    delete agregado

--- bdconexion.php
+++ bdconexion.php
@@ -8,12 +8,12 @@ echo "<center>";
 echo "<a href=''>agregar registro";
 echo "<table border='2'>";
 echo "<tr><th>Nombre</th><th>Correo</th><th>Sueldo</th></tr>";
-$resultado = $conexion->query("SELECT * FROM empleados" . " where sueldo >=12000 AND sueldo<=16000");    
+$resultado = $conexion->query("SELECT * FROM empleados");    
 while($fila = $resultado->fetch_assoc()){
     echo "<tr><td>".$fila['Nombre'] ."</td>";
     echo "<td>".$fila['Correo'] ."</td>"; 
     echo "<td>". $fila['Sueldo']. "</td>";
-    echo "<td><a href='borrar.php ?k='" .$fila['Clave']. "><img src='../bdPrueba/img/basura.png'></a></td></tr>";
+    echo "<td><a href='borrar.php?k=$fila[Clave]'><img src='../bdPrueba/img/basura.png'></a></td></tr>";
 }
 $conexion->close();
 echo"</table>";
--- borrar.php
+++ borrar.php
@@ -1,7 +1,9 @@
 <?php
-   // $conexion = new mysqli("localhost", "root", "", "empresax");
-   // if ($conexion->connect_error) 
-    //die("Error de conexión: " . $conexion->connect_error);
-    echo $_GET['K'];
-    //$resultado=$conexion->query("DELETE FROM empleados WHERE Clave=")
+$conexion = new mysqli("localhost", "root", "", "empresax");
+if ($conexion->connect_error) 
+    die("Error de conexión: " . $conexion->connect_error);
+
+$conexion->query("DELETE FROM empleados WHERE Clave=$_GET[k]");
+$conexion->close();
+header("Location: http://localhost/bdPrueba/bdconexion.php");
 ?>
